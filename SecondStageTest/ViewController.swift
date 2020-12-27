3c2b52a02f7c8f874060d4cc81affd48220037af
Yogi Priyo Prayogo <yogipriyo.prayogo@gmail.com>
2020-12-27 16:18:17 +0700

    first commit

---
+++ SecondStageTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondStageTest
+//
+//  Created by Yogi Priyo Prayogo on 27/12/20.
+//  Copyright © 2020 Yogi Priyo Prayogo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
