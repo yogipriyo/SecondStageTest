5669a9f2141dccf9305787fe342a8521fcede358
Yogi Priyo Prayogo <yogipriyo.prayogo@gmail.com>
2020-12-27 18:01:16 +0700

    removing unneeded files and initiate cocoapod

---
+++ SecondStageTest/Homepage/HomepageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomepageViewController.swift
+//  SecondStageTest
+//
+//  Created by Yogi Priyo Prayogo on 27/12/20.
+//  Copyright © 2020 Yogi Priyo Prayogo. All rights reserved.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
